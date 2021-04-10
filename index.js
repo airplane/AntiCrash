f9d67e525608c456e18b9f0ffa583007776db45e
auto <31675260+airplane@users.noreply.github.com>
2021-04-09 20:06:32 -0400

    Switch to regex

--- index.js
+++ index.js
@@ -4,25 +4,23 @@ const { inject, uninject } = require('powercord/injector');
 
 // Crasher URLs
 let urls = [
-    "https://tornadus.net/orange"
+    "https:\/\/tornadus\.net\/.*" //https://tornadus.net/orange
 ]
 
 module.exports = class AntiCrash extends Plugin {
     async startPlugin() {
 
         const Message = await getModule(m => m.default && m.default.displayName === 'Message');
-        const oldDefault = Message.default;
         inject('anti-crash', Message, 'default', (args, res) => {
-
-            const currentUser = getModule(['getCurrentUser'], false).getCurrentUser();
 
             //Loops through urls and checks if any of them are found in the message.
             urls.forEach(url => {
 
-                const content = args[0]['childrenAccessories']['props']['message']['content'];
+                const content = new String(args[0]['childrenAccessories']['props']['message']['content']);
 
                 //Removes message if url is found.
-                if (content.includes(url)) {
+                if (content.match(url)) {
+                    args[0]['childrenAccessories']['props']['message']['content'] = '';
                     res = '';
 
                     const authorName = args[0]['childrenAccessories']['props']['message']["author"]["username"];
@@ -34,15 +32,11 @@ module.exports = class AntiCrash extends Plugin {
                         header: 'Crasher Detected',
                         timeout: 4e3,
                         content: `Removed. Sent by ${toastContent}`,
-                      });
-
+                    });
                 }
-
             });
             return res
         });
-    
-        Object.assign(Message.default, oldDefault);
     }
 
     pluginWillUnload() {
